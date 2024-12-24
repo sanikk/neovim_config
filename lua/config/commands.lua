50fe39fd518717d862a90e551700a4aee9a289e8
Samuli Nikkilä <sanikk@users.noreply.github.com>
2024-12-24 07:51:26 +0200

    lsp python, statusbar, some commands and options
    commands:
        normal mode when entering neotree window
        autoformat on save
    options:
        sane defaults for indentation
    python:
        pyright, black, flake8 setups and configs
    plugins/statusbar.lua:
        simple setup for lualine

--- lua/config/commands.lua
+++ lua/config/commands.lua
@@ -12,12 +12,6 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
--- Command to load C++ Qt snippets
-vim.api.nvim_create_user_command('LoadCppQtSnippets', function()
-  require('luasnip.loaders.from_lua').load { paths = { '~/.config/nvim/snippets/cpp-qt.lua' } }
-  print 'C++ Qt snippets loaded!'
-end, { desc = 'Load C++ Qt snippets' })
-
 -- Automatically set foldmethod to 'manual' when entering Visual mode
 vim.api.nvim_create_autocmd('ModeChanged', {
   pattern = 'v*', -- Trigger on any visual mode (line-wise, block-wise, etc.)
@@ -33,3 +27,44 @@ vim.api.nvim_create_autocmd({ 'ModeChanged' }, {
     vim.o.foldmethod = 'syntax'
   end,
 })
+
+-- Autoformatter on save
+vim.api.nvim_create_autocmd('BufWritePre', {
+  callback = function()
+    vim.lsp.buf.format { async = false }
+  end,
+})
+
+-- Switch to normal mode when changing focus to neo-tree window
+vim.api.nvim_create_autocmd('BufEnter', {
+  pattern = 'neo-tree://*',
+  callback = function()
+    if vim.fn.mode() ~= 'n' then
+      vim.cmd 'stopinsert'
+    end
+  end,
+})
+-- Manual Commands
+
+-- Command to load C++ Qt snippets
+vim.api.nvim_create_user_command('LoadCppQtSnippets', function()
+  require('luasnip.loaders.from_lua').load { paths = { '~/.config/nvim/snippets/cpp-qt.lua' } }
+  print 'C++ Qt snippets loaded!'
+end, { desc = 'Load C++ Qt snippets' })
+
+-- color border between neotree and code according to mode
+-- local function update_neotree_border_color()
+--   local mode = vim.fn.mode()
+--   local mode_colors = {
+--     n = '#3b82f6', -- Blue for Normal
+--     i = '#16a34a', -- Green for Insert
+--     v = '#7c3aed', -- Purple for Visual
+--     R = '#dc2626', -- Red for Replace
+--   }
+--   local color = mode_colors[mode] or '#ffffff'
+--   vim.api.nvim_set_hl(0, 'NeoTreeBorder', { fg = color })
+-- end
+--
+-- vim.api.nvim_create_autocmd('ModeChanged', {
+--   callback = update_neotree_border_color,
+-- })
