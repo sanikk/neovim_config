83c1af693fdc28a05fdf067dc8e36d76a0c85875
Samuli Nikkilä <sanikk@users.noreply.github.com>
2025-02-12 02:09:35 +0200

    keybind reorg
    moved some keybinds to plugins/keys.lua:
    the submenu names are displayed
    lazygit keys are mostly there now

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,77 +1,75 @@
 return {
-  {
-    'kdheepak/lazygit.nvim',
-    lazy = true,
-    cmd = {
-      'LazyGit',
-      'LazyGitConfig',
-      'LazyGitCurrentFile',
-      'LazyGitFilter',
-      'LazyGitFilterCurrentFile',
-    },
-    -- optional for floating window border decoration
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-    },
-    -- setting the keybinding for LazyGit with 'keys' is recommended in
-    -- order to load the plugin when the command is run for the first time
-    keys = {
-      { '<leader>l', desc = 'LazyGit' },
-      { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
-      {
-        '<leader>lh',
-        function()
-          -- Function logic encapsulated here
-          --           local function lazygit_with_commit_msg()
-          --             -- Open a horizontal split for Lazygit
-          --             vim.cmd 'vsplit'
-          --             -- Open Lazygit in the new split
-          --             vim.cmd 'term lazygit'
-          --             -- Navigate to the other split
-          --             vim.cmd 'wincmd h' -- Adjust to navigate up or down as needed
-          --             -- Open the commit message file
-          --             vim.cmd 'edit .git/COMMIT_EDITMSG'
-          --           end
-          local function open_commit_message_in_magic_workspace()
-            -- Path to the commit message file
-            local cwd = vim.fn.system('pwd'):gsub('\n', '') -- Capture the output of pwd and remove trailing newline
+    {
+        'kdheepak/lazygit.nvim',
+        lazy = true,
+        cmd = {
+            'LazyGit',
+            'LazyGitConfig',
+            'LazyGitCurrentFile',
+            'LazyGitFilter',
+            'LazyGitFilterCurrentFile',
+        },
+        -- optional for floating window border decoration
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+        },
+        -- setting the keybinding for LazyGit with 'keys' is recommended in
+        -- order to load the plugin when the command is run for the first time
+        keys = {
+            {
+                '<leader>lh',
+                function()
+                    -- Function logic encapsulated here
+                    --           local function lazygit_with_commit_msg()
+                    --             -- Open a horizontal split for Lazygit
+                    --             vim.cmd 'vsplit'
+                    --             -- Open Lazygit in the new split
+                    --             vim.cmd 'term lazygit'
+                    --             -- Navigate to the other split
+                    --             vim.cmd 'wincmd h' -- Adjust to navigate up or down as needed
+                    --             -- Open the commit message file
+                    --             vim.cmd 'edit .git/COMMIT_EDITMSG'
+                    --           end
+                    local function open_commit_message_in_magic_workspace()
+                        -- Path to the commit message file
+                        local cwd = vim.fn.system('pwd'):gsub('\n', '') -- Capture the output of pwd and remove trailing newline
 
-            -- Full path to the commit message file
-            local commit_message_file = cwd .. '/.git/COMMIT_EDITMSG'
-            -- local commit_message_file = '.git/COMMIT_EDITMSG'
+                        -- Full path to the commit message file
+                        local commit_message_file = cwd .. '/.git/COMMIT_EDITMSG'
+                        -- local commit_message_file = '.git/COMMIT_EDITMSG'
 
-            -- Hyprctl command to open Neovim with the commit message file in the magic workspace
-            local cmd = string.format(
-              'hyprctl dispatch exec "kitty --class=specialMagicTerm nvim %s"; sleep 0.2; hyprctl dispatch movetoworkspace special:magic,class:specialMagicTerm',
-              commit_message_file
-            )
+                        -- Hyprctl command to open Neovim with the commit message file in the magic workspace
+                        local cmd = string.format(
+                            'hyprctl dispatch exec "kitty --class=specialMagicTerm nvim %s"; sleep 0.2; hyprctl dispatch movetoworkspace special:magic,class:specialMagicTerm',
+                            commit_message_file
+                        )
 
-            -- Execute the command
-            os.execute(cmd)
-          end
-          -- Call the function
-          -- lazygit_with_commit_msg()
-          open_commit_message_in_magic_workspace()
-        end,
-        desc = 'Open Lazygit with commit message split',
-      },
+                        -- Execute the command
+                        os.execute(cmd)
+                    end
+                    -- Call the function
+                    -- lazygit_with_commit_msg()
+                    open_commit_message_in_magic_workspace()
+                end,
+                desc = 'Open Lazygit with commit message split',
+            },
+        },
     },
-  },
-  -- Here is a more advanced example where we pass configuration
-  -- options to `gitsigns.nvim`. This is equivalent to the following Lua:
-  --    require('gitsigns').setup({ ... })
-  --
-  -- See `:help gitsigns` to understand what the configuration keys do
-  { -- Adds git related signs to the gutter, as well as utilities for managing changes
-    'lewis6991/gitsigns.nvim',
-    opts = {
-      signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
-      },
+    -- Here is a more advanced example where we pass configuration
+    -- options to `gitsigns.nvim`. This is equivalent to the following Lua:
+    --    require('gitsigns').setup({ ... })
+    --
+    -- See `:help gitsigns` to understand what the configuration keys do
+    { -- Adds git related signs to the gutter, as well as utilities for managing changes
+        'lewis6991/gitsigns.nvim',
+        opts = {
+            signs = {
+                add = { text = '+' },
+                change = { text = '~' },
+                delete = { text = '_' },
+                topdelete = { text = '‾' },
+                changedelete = { text = '~' },
+            },
+        },
     },
-  },
 }
--- lua/plugins/keys.lua
+++ lua/plugins/keys.lua
@@ -1,32 +1,37 @@
 return {
-    'folke/which-key.nvim',
-    event = 'VeryLazy',
-    opts = {
-        -- Your which-key options here
+  'folke/which-key.nvim',
+  event = 'VeryLazy',
+  opts = {
+    -- Your which-key options here
+  },
+  keys = {
+    {
+      '<leader>?',
+      function()
+        require('which-key').show { global = false }
+      end,
+      desc = 'Buffer Local Keymaps (which-key)',
     },
-    keys = {
-        {
-            '<leader>?',
-            function()
-                require('which-key').show { global = false }
-            end,
-            desc = 'Buffer Local Keymaps (which-key)',
-        },
 
-        { '<leader>l',  desc = 'LazyGit' },
-        { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
-        {
-            '<leader>lh',
-            function()
-                -- Your custom function here
-            end,
-            desc = 'LazyGit Custom Function',
-        },
-        { '<leader>b',  desc = 'Buffers' },
-        { '<leader>bd', '<cmd>bdelete<cr>', desc = 'Close current buffer' },
+    { '<leader>l', '<nop>', desc = 'LazyGit' },
+    { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
+    {
+      '<leader>lh',
+      function()
+        -- Your custom function here
+      end,
+      desc = 'LazyGit Custom Function',
     },
-    config = function(_, opts)
-        require('which-key').setup(opts)
-        -- Additional setup if needed
-    end,
+
+    { '<leader>b', '<nop>', desc = 'Buffers' },
+    { '<leader>bd', '<cmd>bdelete<cr>', desc = 'Close current buffer' },
+
+    { '<leader>s', '<nop>', desc = 'Search' },
+
+    { '<leader>d', '<nop>', desc = 'Debug' },
+  },
+  config = function(_, opts)
+    require('which-key').setup(opts)
+    -- Additional setup if needed
+  end,
 }
