1762616b6595619e05cbd7ee09960cb09eb87d11
Samuli Nikkilä <sanikk@users.noreply.github.com>
2025-03-06 01:45:58 +0200

    friendly-snippets plugin

--- lua/plugins/autocompletion.lua
+++ lua/plugins/autocompletion.lua
@@ -20,12 +20,19 @@ return {
           -- `friendly-snippets` contains a variety of premade snippets.
           --    See the README about individual language/framework/plugin snippets:
           --    https://github.com/rafamadriz/friendly-snippets
-          -- {
-          --   'rafamadriz/friendly-snippets',
-          --   config = function()
-          --     require('luasnip.loaders.from_vscode').lazy_load()
-          --   end,
-          -- },
+          {
+            'rafamadriz/friendly-snippets',
+            config = function()
+              require('luasnip.loaders.from_vscode').lazy_load {
+                exclude = { '*' }, -- First, exclude everything
+              }
+
+              require('luasnip.loaders.from_vscode').lazy_load {
+                include = { 'cpp', 'python' }, -- Then, explicitly load what you need
+              }
+              -- require('luasnip.loaders.from_vscode').lazy_load()
+            end,
+          },
         },
       },
       'saadparwaiz1/cmp_luasnip',
