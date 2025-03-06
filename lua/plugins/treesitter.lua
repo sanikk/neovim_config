4fadfef674e3d17163b00372656dc240d4c7f797
Samuli Nikkilä <sanikk@users.noreply.github.com>
2025-03-06 03:20:03 +0200

    remove treesitter/playground
    it's deprecated
    :Inspect does the trick

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,16 +6,22 @@ return {
     opts = {
       ensure_installed = { 'python', 'bash', 'c', 'html', 'lua', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
       auto_install = true,
-      highlight = { enable = true },
-      indent = { enable = true },
+      highlight = {
+        enable = true,
+        custom_captures = {
+          -- Define a custom capture for the print statements
+          pythonPrint = 'PythonPrint',
+        },
+      },
+      indent = {
+        enable = true,
+        -- disable = { 'comment' },
+      },
       playground = {
         enable = true,
         updatetime = 25, -- Debounced time for highlighting nodes
         persist_queries = false, -- Disable persistent queries across sessions
       },
     },
-  },
-  { -- Treesitter Playground extension
-    'nvim-treesitter/playground',
   },
 }
