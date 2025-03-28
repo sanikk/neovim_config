edabc5938478e149efaf2ce563a7af5c1a6faeb0
Samuli Nikkilä <sanikk@users.noreply.github.com>
2025-03-28 23:23:01 +0200

    keymap edit: change to next/prev buffer to use visual order

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -33,29 +33,33 @@ vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower win
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
 -- buffers
-vim.keymap.set('n', '<S-h>', '<cmd>bprevious<cr>', { desc = 'Prev Buffer' })
-vim.keymap.set('n', '<S-l>', '<cmd>bnext<cr>', { desc = 'Next Buffer' })
+-- original setup
+-- vim.keymap.set('n', '<S-h>', '<cmd>bprevious<cr>', { desc = 'Prev Buffer' })
+-- vim.keymap.set('n', '<S-l>', '<cmd>bnext<cr>', { desc = 'Next Buffer' })
+-- modified for BufferLine to use visible order
+vim.keymap.set('n', '<S-h>', '<cmd>BufferLineCyclePrev<cr>', { desc = 'Prev Buffer' })
+vim.keymap.set('n', '<S-l>', '<cmd>BufferLineCycleNext<cr>', { desc = 'Next Buffer' })
 
 -- DAP keymaps
 do
-    local dap = require 'dap'
+  local dap = require 'dap'
 
-    local mappings = {
-        { mode = 'n', lhs = '<leader>db', rhs = dap.toggle_breakpoint, desc = 'Toggle Breakpoint' },
-        { mode = 'n', lhs = '<leader>dc', rhs = dap.continue,          desc = 'Continue' },
-        { mode = 'n', lhs = '<leader>di', rhs = dap.step_into,         desc = 'Step Into' },
-        { mode = 'n', lhs = '<leader>do', rhs = dap.step_over,         desc = 'Step Over' },
-        {
-            mode = 'n',
-            lhs = '<leader>dr',
-            rhs = function()
-                dap.repl.toggle()
-            end,
-            desc = 'Toggle REPL',
-        },
-    }
+  local mappings = {
+    { mode = 'n', lhs = '<leader>db', rhs = dap.toggle_breakpoint, desc = 'Toggle Breakpoint' },
+    { mode = 'n', lhs = '<leader>dc', rhs = dap.continue, desc = 'Continue' },
+    { mode = 'n', lhs = '<leader>di', rhs = dap.step_into, desc = 'Step Into' },
+    { mode = 'n', lhs = '<leader>do', rhs = dap.step_over, desc = 'Step Over' },
+    {
+      mode = 'n',
+      lhs = '<leader>dr',
+      rhs = function()
+        dap.repl.toggle()
+      end,
+      desc = 'Toggle REPL',
+    },
+  }
 
-    for _, map in ipairs(mappings) do
-        vimkey(map.mode, map.lhs, map.rhs, { desc = map.desc })
-    end
+  for _, map in ipairs(mappings) do
+    vimkey(map.mode, map.lhs, map.rhs, { desc = map.desc })
+  end
 end
