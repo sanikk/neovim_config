3f70fc320d5ac13aadbb21869c86aeb8859279db
Samuli Nikkilä <sanikk@users.noreply.github.com>
2025-04-11 04:24:17 +0300

    todo-comments: keybinds

--- lua/plugins/keys.lua
+++ lua/plugins/keys.lua
@@ -1,37 +1,45 @@
 return {
-  'folke/which-key.nvim',
-  event = 'VeryLazy',
-  opts = {
-    -- Your which-key options here
-  },
-  keys = {
-    {
-      '<leader>?',
-      function()
-        require('which-key').show { global = false }
-      end,
-      desc = 'Buffer Local Keymaps (which-key)',
+    'folke/which-key.nvim',
+    event = 'VeryLazy',
+    opts = {
+        -- Your which-key options here
     },
+    keys = {
+        {
+            '<leader>?',
+            function()
+                require('which-key').show { global = false }
+            end,
+            desc = 'Buffer Local Keymaps (which-key)',
+        },
 
-    { '<leader>l', '<nop>', desc = 'LazyGit' },
-    { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
-    {
-      '<leader>lh',
-      function()
-        -- Your custom function here
-      end,
-      desc = 'LazyGit Custom Function',
+        { '<leader>l',  '<nop>',            desc = 'LazyGit' },
+        { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
+        {
+            '<leader>lh',
+            function()
+                -- Your custom function here
+            end,
+            desc = 'LazyGit Custom Function',
+        },
+
+        { '<leader>b',  '<nop>',                       desc = 'Buffers' },
+        { '<leader>bd', '<cmd>bdelete<cr>',            desc = 'Close current buffer' },
+        { '<leader>bh', '<cmd>BufferLineMovePrev<cr>', desc = 'Move current buffer left' },
+        { '<leader>bl', '<cmd>BufferLineMoveNext<cr>', desc = 'Move current buffer right' },
+
+        { '<leader>s',  '<nop>',                       desc = 'Search' },
+
+        { '<leader>d',  '<nop>',                       desc = 'Debug' },
+
+        { '<leader>t',  '<nop>',                       desc = 'Toggle' },
+
+        { '<leader>p',  '<nop>',                       desc = 'Project' },
+        { '<leader>pt', '<cmd>TodoQuickFix<cr>',       desc = 'List project TODOs' },
+        -- { '<leader>pk',
     },
-
-    { '<leader>b', '<nop>', desc = 'Buffers' },
-    { '<leader>bd', '<cmd>bdelete<cr>', desc = 'Close current buffer' },
-
-    { '<leader>s', '<nop>', desc = 'Search' },
-
-    { '<leader>d', '<nop>', desc = 'Debug' },
-  },
-  config = function(_, opts)
-    require('which-key').setup(opts)
-    -- Additional setup if needed
-  end,
+    config = function(_, opts)
+        require('which-key').setup(opts)
+        -- Additional setup if needed
+    end,
 }
